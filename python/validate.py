b71bb1319476e6f9c4106f6a72bd6ed7e72877a0
eezstreet <eezstreet@live.com>
2026-02-26 13:54:20 -0500

    More whack-a-mole

--- python/validate.py
+++ python/validate.py
@@ -30,8 +30,8 @@ def importJs(jsPath):
             files[key] = file
 
 def importFieldExport(fieldExportPath):
-    if not fieldExportPath.exists(): # I'll try making the filename lowercase. That's a neat trick!
-        fieldExportPath = fieldExportPath.with_name(fieldExportPath.name.lower())
+    if not os.path.exists(fieldExportPath):
+        fieldExportPath = Path(fieldExportPath).with_name(Path(fieldExportPath).name.lower())
     with io.open(fieldExportPath, 'r') as fieldExportFile:
         jsonText = fieldExportFile.read()
         global allFieldExports
